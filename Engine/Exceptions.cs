1f8681896cfc623914fff880dfff8b25d0fde78b
Blair Learn <blairlearn@users.noreply.github.com>
2011-04-14 21:32:09 +0000

    Define RelationshipException.
    Make all migration-related exceptions derive from MigrationException.

--- Engine/Exceptions.cs
+++ Engine/Exceptions.cs
@@ -2,13 +2,47 @@
 
 namespace MigrationEngine
 {
+    /// <summary>
+    /// Base class for exceptions in the MigrationEngine.
+    /// </summary>
     [global::System.Serializable]
-    public class ConfigurationException : Exception
+    public abstract class MigrationException : Exception
     {
-        public ConfigurationException() { }
-        public ConfigurationException(string message) : base(message) { }
-        public ConfigurationException(string message, Exception inner) : base(message, inner) { }
-        protected ConfigurationException(
+        public MigrationException() { }
+        public MigrationException(string message) : base(message) { }
+        public MigrationException(string message, Exception inner) : base(message, inner) { }
+        protected MigrationException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context)
+            : base(info, context) { }
+    }
+
+    /// <summary>
+    /// Exception thrown when errors are encountered in migration script.
+    /// (e.g. Required attribute not being set.)
+    /// </summary>
+    [global::System.Serializable]
+    public class MigrationScriptException : MigrationException
+    {
+        public MigrationScriptException() { }
+        public MigrationScriptException(string message) : base(message) { }
+        public MigrationScriptException(string message, Exception inner) : base(message, inner) { }
+        protected MigrationScriptException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context)
+            : base(info, context) { }
+    }
+
+    /// <summary>
+    /// Exception thrown when unable to create a relationship between content items.
+    /// </summary>
+    [global::System.Serializable]
+    public class RelationshipException : MigrationException
+    {
+        public RelationshipException() { }
+        public RelationshipException(string message) : base(message) { }
+        public RelationshipException(string message, Exception inner) : base(message, inner) { }
+        protected RelationshipException(
           System.Runtime.Serialization.SerializationInfo info,
           System.Runtime.Serialization.StreamingContext context)
             : base(info, context) { }
