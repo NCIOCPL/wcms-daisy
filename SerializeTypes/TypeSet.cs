a8eac31e89716893dd66e32cd7c7f78f741dc56f
Blair Learn <blairlearn@users.noreply.github.com>
2011-05-25 20:32:39 +0000

    Create test program for serializing sample migration script elements.

---
+++ SerializeTypes/TypeSet.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using MigrationEngine.Descriptors;
+using MigrationEngine.DataAccess;
+using MigrationEngine.Tasks;
+
+namespace SerializeTypes
+{
+    public class TypeSet
+    {
+        public MigrationTask[] MigrationTaskList;
+
+        public TypeSet()
+        {
+            MigrationTaskList = new MigrationTask[]
+        {
+            new FolderCreator(),
+            new GeneralContentCreator(),
+            new ContentUpdater(){ DataGetter = new DatabaseDataGetter<UpdateContentItem>() },
+            new Transitioner()
+        };
+        }
+    }
+}
