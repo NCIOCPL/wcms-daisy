06b9bf09ffe3f9dd46fcf53469346d3c9a17dcec
Blair Learn <blairlearn@users.noreply.github.com>
2011-06-14 16:45:17 +0000

    XmlRelationshipDescriptionMapper.cs   XML Data Mapper for relationships.

---
+++ Engine/Mappers/XmlRelationshipDescriptionMapper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+using MigrationEngine.Descriptors;
+
+namespace MigrationEngine.Mappers
+{
+    public class XmlRelationshipDescriptionMapper
+        : XmlDataMapper<RelationshipDescription>
+    {
+        public override RelationshipDescription MapItem(object dataItem)
+        {
+            if (!(dataItem is XmlNode))
+                throw new ArgumentException(string.Format("Parameter dataItem is of type {0}, expected XmlNode."),
+                    dataItem.GetType().Name);
+
+            XmlNode item = (XmlNode)dataItem;
+
+            RelationshipDescription description = new RelationshipDescription();
+
+            description.OwnerMigrationID = new Guid(GetNamedFieldValue(item, "ownerid", FieldMappingErrorHandler));
+            description.OwnerContentType = GetNamedFieldValue(item, "ownercontenttype", FieldMappingErrorHandler);
+
+            description.DependentMigrationID = new Guid(GetNamedFieldValue(item, "dependentid", FieldMappingErrorHandler));
+            description.DependentContentType = GetNamedFieldValue(item, "dependentcontenttype", FieldMappingErrorHandler);
+
+            description.SlotName = GetNamedFieldValue(item, "slot", FieldMappingErrorHandler);
+            description.TemplateName = GetNamedFieldValue(item, "template", FieldMappingErrorHandler);
+
+            CheckForRecordedErrors();
+
+            return description;
+        }
+    }
+}
