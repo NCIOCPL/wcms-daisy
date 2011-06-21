18f143a330db887ff9bfe08106171ba68aa685d7
Blair Learn <blairlearn@users.noreply.github.com>
2011-06-21 16:27:21 +0000

    nciImage.cs   Rename img_alt_txt field to img_alt   Stop setting bogus data for fields which are now optional.

--- FileManipulation/nciImage.cs
+++ FileManipulation/nciImage.cs
@@ -29,23 +29,7 @@ namespace FileManipulation
             _fieldSet.Add("img1_width", info.Width.ToString());
             _fieldSet.Add("img1_type", info.MimeType);
 
-            _fieldSet.Add("img2", info.Data);
-            _fieldSet.Add("img2_ext", info.Extension);
-            _fieldSet.Add("img2_filename", info.FileName);
-            _fieldSet.Add("img2_size", info.FileSize.ToString());
-            _fieldSet.Add("img2_height", info.Height.ToString());
-            _fieldSet.Add("img2_width", info.Width.ToString());
-            _fieldSet.Add("img2_type", info.MimeType);
-
-            _fieldSet.Add("img3", info.Data);
-            _fieldSet.Add("img3_ext", info.Extension);
-            _fieldSet.Add("img3_filename", info.FileName);
-            _fieldSet.Add("img3_size", info.FileSize.ToString());
-            _fieldSet.Add("img3_height", info.Height.ToString());
-            _fieldSet.Add("img3_width", info.Width.ToString());
-            _fieldSet.Add("img3_type", info.MimeType);
-
-            _fieldSet.Add("img_alt_txt", altText);
+            _fieldSet.Add("img_alt", altText);
         }
     }
 }
