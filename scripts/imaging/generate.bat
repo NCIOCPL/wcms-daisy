9f16df47fd37f3e4ab2d1e3f19902fe40a94b060
Blair Learn <blairlearn@users.noreply.github.com>
2011-07-06 21:18:20 +0000

    Simple generation script.

---
+++ scripts/imaging/generate.bat
@@ -0,0 +1,2 @@
+svn --force export https://imbncipf01.nci.nih.gov/svn/wcmteam/ContentDeliveryEngine/branches/6.2/CDETemplates/StandardCDETemplates/PageTemplates PageTemplates
+..\..\Blu82\bin\Debug\blu82.exe imaging_inputs.xls
