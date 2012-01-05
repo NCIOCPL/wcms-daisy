66e861e132ff5881f169cdf27b01f1d71f5660e4
Blair Learn <blairlearn@users.noreply.github.com>
2012-01-05 18:44:58 +0000

    Implement LinkToFolder task allowing content items to be associated with additional folders.

---
+++ Migration/Engine/Mappers/XmlFolderLinkDescriptionMapper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+using MigrationEngine.Descriptors;
+
+namespace MigrationEngine.Mappers
+{
+    public class XmlFolderLinkDescriptionMapper
+        :XmlDataMapper<FolderLinkDescription>
+    {
+        public override FolderLinkDescription MapItem(object dataItem)
+        {
+            if (!(dataItem is XmlNode))
+                throw new ArgumentException(string.Format("Parameter dataItem is of type {0}, expected XmlNode."),
+                    dataItem.GetType().Name);
+
+            XmlNode item = (XmlNode)dataItem;
+
+            FolderLinkDescription description = new FolderLinkDescription();
+
+            try
+            {
+                description.Path = GetNamedFieldValue(item, PathNameField);
+                description.ObjectMonikerName = GetNamedFieldValue(item, "monikername");
+
+                CopyFields(item, description.Fields);
+            }
+            finally
+            {
+                CheckForRecordedErrors();
+            }
+
+            return description;
+        }
+    }
+}
