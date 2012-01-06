90de0c8a86ab43ee3ca59a04076974c1f16cab31
Blair Learn <blairlearn@users.noreply.github.com>
2012-01-06 18:52:29 +0000

    Test inputs for CancerGov mobile

---
+++ Input Generator/scripts/mobile/generate.bat
@@ -0,0 +1,3 @@
+rem svn --force export https://imbncipf01.nci.nih.gov/svn/wcmteam/ContentDeliveryEngine/branches/6.2/CDETemplates/StandardCDETemplates/PageTemplates PageTemplates
+rem svn --force export https://imbncipf01.nci.nih.gov/svn/WCMTeam/ContentDeliveryEngine/branches/6.2/CDETemplates/Proteomics Resources
+..\..\Blu82\bin\Debug\blu82.exe proteomics_inputs.xls
