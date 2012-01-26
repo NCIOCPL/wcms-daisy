c281b9d1f39bcf0c4e2a26cde80cc5844d27ad5d
Blair Learn <blairlearn@users.noreply.github.com>
2012-01-26 19:07:06 +0000

    Set location for download files.

--- Input Generator/scripts/mobile/configuration/generate.bat
+++ Input Generator/scripts/mobile/configuration/generate.bat
@@ -1,6 +1,6 @@
-svn --force export https://imbncipf01/svn/WCMTeam/content/branches/6.4/CancerGov-Mobile/Scripts js
-svn --force export https://imbncipf01/svn/WCMTeam/content/branches/6.4/CancerGov-Mobile/Stylesheets css
-svn --force export https://imbncipf01/svn/WCMTeam/content/branches/6.4/CancerGov-Mobile/PageTemplates templates
-svn --force export https://imbncipf01/svn/WCMTeam/content/branches/6.4/CancerGov-Mobile/Images images
-svn --force export https://imbncipf01/svn/WCMTeam/content/branches/6.4/_CommonContent/global/JS js
-..\..\Blu82\bin\Debug\blu82.exe mobile_inputs.xls
+svn --force export https://imbncipf01/svn/WCMTeam/content/branches/6.4/CancerGov-Mobile/Scripts "C:\temp\Input Generator\scripts\mobile\js"
+svn --force export https://imbncipf01/svn/WCMTeam/content/branches/6.4/CancerGov-Mobile/Stylesheets "C:\temp\Input Generator\scripts\mobile\css"
+svn --force export https://imbncipf01/svn/WCMTeam/content/branches/6.4/CancerGov-Mobile/PageTemplates "C:\temp\Input Generator\scripts\mobile\templates"
+svn --force export https://imbncipf01/svn/WCMTeam/content/branches/6.4/CancerGov-Mobile/Images "C:\temp\Input Generator\scripts\mobile\images"
+svn --force export https://imbncipf01/svn/WCMTeam/content/branches/6.4/_CommonContent/global/JS "C:\temp\Input Generator\scripts\mobile\js"
+..\..\..\Blu82\bin\Debug\blu82.exe mobile_configuration_inputs.xls
