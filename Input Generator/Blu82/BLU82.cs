75459dbff661d59c9cb1cd679707e6214523e6f1
Bryan Pizzillo <bryanpizzillo@users.noreply.github.com>
2015-02-23 15:07:10 +0000

    - Added in changes to support multiple content type tabs OCEPROJECT-350 - Added in NVCG configuration spreadsheets

--- Input Generator/Blu82/BLU82.cs
+++ Input Generator/Blu82/BLU82.cs
@@ -36,41 +36,47 @@ namespace Blu82
             }
             SerializeCollection<DaisyFolder>(folderList, @".\folder.xml");
 
-            var siteColumnNames = excel.GetColumnNames("2 Content Items");
+            // ------------------
+            // Content Extraction
+            // ------------------
 
-            //Get Site content items            
-            var siteContentitems = from contentitem in excel.Worksheet("2 Content Items")
-                                   where contentitem["community"] == "site"
-                                   select contentitem;
-            List<DaisyContentItem> siteCommunity = ExtractItems(siteColumnNames, siteContentitems, "site");
-            siteCommunity.ForEach(entry => {
-                if(!entry.folder.StartsWith("/"))
-                    entry.folder = "/" + entry.folder.Trim();
-            });
-            SerializeCollection<DaisyContentItem>(siteCommunity, @".\siteContentItems.xml");
+            //1. Setup Lists for site content
+            List<DaisyContentItem> siteCommunity = new List<DaisyContentItem>();
+            List<DaisyContentItem> siteAdminCommunity = new List<DaisyContentItem>();
+            List<DaisyContentItem> ctbAdminCommunity = new List<DaisyContentItem>();
 
-            //Get Site Admin content items
-            var siteAdminContentitems = from contentitem in excel.Worksheet("2 Content Items")
-                                        where contentitem["community"] == "siteAdmin"
-                                        select contentitem;
-            List<DaisyContentItem> siteAdminCommunity = ExtractItems(siteColumnNames, siteAdminContentitems, "siteAdmin");
-            siteAdminCommunity.ForEach(entry =>
+            //2. Loop through each sheet to find content sheets
+            foreach (String sheetName in excel.GetWorksheetNames())
             {
-                if (!entry.folder.StartsWith("/"))
-                    entry.folder = "/" + entry.folder.Trim();
-            });
+                //All content item sheets will start with "2 CT"
+                if (sheetName.ToLower().StartsWith("2 CT"))
+                {
+
+                    //String contentType = sheetName.Replace("2 CT", "").Trim();
+
+
+                    //We should probably check to make sure that content_type is not null, but 
+                    //nothing else in this file generates errors.  This would end up being a
+                    //validation error for Daisy input.
+
+                    siteCommunity.AddRange(
+                        ExtractItemsForCommunity(excel, sheetName, "site")                    
+                        );
+
+                    siteAdminCommunity.AddRange(
+                        ExtractItemsForCommunity(excel, sheetName, "siteAdmin")
+                        );
+
+                    ctbAdminCommunity.AddRange(
+                        ExtractItemsForCommunity(excel, sheetName, "ctbAdmin")
+                        );
+                }
+            }
+            
+            //Now that we have looped through all the content sheets, output the Daisy Inputs
+
+            SerializeCollection<DaisyContentItem>(siteCommunity, @".\siteContentItems.xml");           
             SerializeCollection<DaisyContentItem>(siteAdminCommunity, @".\siteAdminContentItems.xml");
-
-            //Get CTB Admin content items
-            var ctbAdminContentitems = from contentitem in excel.Worksheet("2 Content Items")
-                                       where contentitem["community"] == "ctbAdmin"
-                                       select contentitem;
-            List<DaisyContentItem> ctbAdminCommunity = ExtractItems(siteColumnNames, ctbAdminContentitems, "ctbAdmin");
-            ctbAdminCommunity.ForEach(entry =>
-            {
-                if (!entry.folder.StartsWith("/"))
-                    entry.folder = "/" + entry.folder.Trim();
-            });
             SerializeCollection<DaisyContentItem>(ctbAdminCommunity, @".\ctbAdminContentItems.xml");
 
             // Get Share to Folders
@@ -92,17 +98,31 @@ namespace Blu82
             Console.Read();
         }
 
-        private static void SerializeCollection<T>(IEnumerable<T> objToSer, String fileName)
+        /// <summary>
+        /// Extracts all the items from a sheet that are of a certain content type within a community
+        /// </summary>
+        /// <param name="excel">The excel file</param>
+        /// <param name="sheetName">The name of the sheet</param>
+        /// <param name="contentType">The content type</param>
+        /// <param name="siteColumnNames"></param>
+        /// <param name="community">The Community to get content for (site, siteAdmin, or ctbadmin)</param>
+        /// <returns></returns>
+        private static List<DaisyContentItem> ExtractItemsForCommunity(ExcelQueryFactory excel, String sheetName, string community)
         {
-            DaisyCollection<T> collection = new DaisyCollection<T>() { Items = objToSer.ToArray() };
+            var siteColumnNames = excel.GetColumnNames(sheetName);
 
-            XmlSerializer serializer = new XmlSerializer(typeof(DaisyCollection<T>), new XmlRootAttribute("list"));
-            XmlSerializerNamespaces ns = new XmlSerializerNamespaces();
-            ns.Add("", "");
+            List<DaisyContentItem> items = ExtractItems(
+                                            siteColumnNames,
+                                            (
+                                                //LINQ for all rows of a community
+                                                from contentitem in excel.Worksheet(sheetName)
+                                                where contentitem["community"] == community
+                                                select contentitem
+                                            ), 
+                                            community
+                                        );
 
-            TextWriter writer = new StreamWriter(fileName);
-            serializer.Serialize(writer, collection, ns);
-            writer.Close();
+            return items;
         }
 
         /// <summary>
@@ -125,60 +145,10 @@ namespace Blu82
                         community = community,
                         mig_id = row["mig_id"],
                         contenttype = row["contenttype"],
-                        folder = row["folder"]
+                        folder = CleanFolder(row["folder"])
                     };
 
-                    //Add additional unknown fields
-                    foreach (String columnName in columnNames.Where(name => { return name != "community" && name != "mig_id" && name != "contenttype" && name != "folder"; }))
-                    {
-                        string val = row[columnName];
-                        if (!string.IsNullOrEmpty(val))
-                        {
-                            if (columnName.StartsWith("file:", StringComparison.OrdinalIgnoreCase))
-                            {
-                                string fileFieldName = columnName.Remove(0, 5);
-                                WebFileInfo fileInfo = WebFileInfo.DownloadFile(val);
-
-                                item.Fields.Add(fileFieldName, fileInfo.Data);
-                                item.Fields.Add(fileFieldName + "_ext", fileInfo.Extension);
-                                item.Fields.Add(fileFieldName + "_filename", fileInfo.FileName);
-                                item.Fields.Add(fileFieldName + "_size", fileInfo.FileSize.ToString());
-                                item.Fields.Add(fileFieldName + "_type", fileInfo.MimeType);
-
-                                //item_file_attachment
-                                //item_file_attachment_ext
-                                //item_file_attachment_filename
-                                //item_file_attachment_size
-                                //item_file_attachment_type
-                            }
-                            else if (columnName.StartsWith("image:", StringComparison.OrdinalIgnoreCase))
-                            {
-                                string imageFieldName = columnName.Remove(0, 6);
-                                ImageInfo fileInfo = ImageInfo.DownloadImage(val);
-
-                                item.Fields.Add(imageFieldName, fileInfo.Data);
-                                item.Fields.Add(imageFieldName + "_ext", fileInfo.Extension);
-                                item.Fields.Add(imageFieldName + "_filename", fileInfo.FileName);
-                                item.Fields.Add(imageFieldName + "_size", fileInfo.FileSize.ToString());
-                                item.Fields.Add(imageFieldName + "_type", fileInfo.MimeType);
-                                item.Fields.Add(imageFieldName + "_width", fileInfo.Width.ToString());
-                                item.Fields.Add(imageFieldName + "_height", fileInfo.Height.ToString());
-
-                                //img1
-                                //img1_ext
-                                //img1_filename
-                                //img1_height
-                                //img1_size
-                                //img1_type
-                                //img1_width
-
-                            }
-                            else
-                            {
-                                item.Fields.Add(columnName, val);
-                            }
-                        }
-                    }
+                    PopulateItemFields(columnNames, row, item);
 
                     rtnItems.Add(item);
                 }
@@ -186,6 +156,83 @@ namespace Blu82
 
             return rtnItems;
         }
+
+        private static string CleanFolder(string folder)
+        {
+            if (!folder.StartsWith("/"))
+                return "/" + folder.Trim();
+            else
+                return folder;
+        }
+
+        private static void PopulateItemFields(IEnumerable<string> columnNames, Row row, DaisyContentItem item)
+        {
+            //Add additional unknown fields
+            foreach (String columnName in columnNames.Where(name => { return name != "community" && name != "mig_id" && name != "contenttype" && name != "folder"; }))
+            {
+                string val = row[columnName];
+                if (!string.IsNullOrEmpty(val))
+                {
+                    if (columnName.StartsWith("file:", StringComparison.OrdinalIgnoreCase))
+                    {
+                        string fileFieldName = columnName.Remove(0, 5);
+                        WebFileInfo fileInfo = WebFileInfo.DownloadFile(val);
+
+                        item.Fields.Add(fileFieldName, fileInfo.Data);
+                        item.Fields.Add(fileFieldName + "_ext", fileInfo.Extension);
+                        item.Fields.Add(fileFieldName + "_filename", fileInfo.FileName);
+                        item.Fields.Add(fileFieldName + "_size", fileInfo.FileSize.ToString());
+                        item.Fields.Add(fileFieldName + "_type", fileInfo.MimeType);
+
+                        //item_file_attachment
+                        //item_file_attachment_ext
+                        //item_file_attachment_filename
+                        //item_file_attachment_size
+                        //item_file_attachment_type
+                    }
+                    else if (columnName.StartsWith("image:", StringComparison.OrdinalIgnoreCase))
+                    {
+                        string imageFieldName = columnName.Remove(0, 6);
+                        ImageInfo fileInfo = ImageInfo.DownloadImage(val);
+
+                        item.Fields.Add(imageFieldName, fileInfo.Data);
+                        item.Fields.Add(imageFieldName + "_ext", fileInfo.Extension);
+                        item.Fields.Add(imageFieldName + "_filename", fileInfo.FileName);
+                        item.Fields.Add(imageFieldName + "_size", fileInfo.FileSize.ToString());
+                        item.Fields.Add(imageFieldName + "_type", fileInfo.MimeType);
+                        item.Fields.Add(imageFieldName + "_width", fileInfo.Width.ToString());
+                        item.Fields.Add(imageFieldName + "_height", fileInfo.Height.ToString());
+
+                        //img1
+                        //img1_ext
+                        //img1_filename
+                        //img1_height
+                        //img1_size
+                        //img1_type
+                        //img1_width
+
+                    }
+                    else
+                    {
+                        item.Fields.Add(columnName, val);
+                    }
+                }
+            }
+        }
+
+        private static void SerializeCollection<T>(IEnumerable<T> objToSer, String fileName)
+        {
+            DaisyCollection<T> collection = new DaisyCollection<T>() { Items = objToSer.ToArray() };
+
+            XmlSerializer serializer = new XmlSerializer(typeof(DaisyCollection<T>), new XmlRootAttribute("list"));
+            XmlSerializerNamespaces ns = new XmlSerializerNamespaces();
+            ns.Add("", "");
+
+            TextWriter writer = new StreamWriter(fileName);
+            serializer.Serialize(writer, collection, ns);
+            writer.Close();
+        }
+
 
         static void Main(string[] args)
         {
---
+++ Input Generator/scripts/NVCG/configuration/generate.bat
@@ -0,0 +1,9 @@
+REM -------------------------------------------------------
+REM Update these paths in SVN to properly build the inputs.
+REM -------------------------------------------------------
+REM svn --force export https://imbncipf01/svn/WCMTeam/content/branches/6.4/CancerGov-Mobile/Scripts "C:\temp\Input Generator\scripts\mobile\js"
+REM svn --force export https://imbncipf01/svn/WCMTeam/content/branches/6.4/CancerGov-Mobile/Stylesheets "C:\temp\Input Generator\scripts\mobile\css"
+REM svn --force export https://imbncipf01/svn/WCMTeam/content/branches/6.4/CancerGov-Mobile/PageTemplates "C:\temp\Input Generator\scripts\mobile\templates"
+REM svn --force export https://imbncipf01/svn/WCMTeam/content/branches/6.4/CancerGov-Mobile/Images "C:\temp\Input Generator\scripts\mobile\images"
+REM svn --force export https://imbncipf01/svn/WCMTeam/content/branches/6.4/_CommonContent/global/JS "C:\temp\Input Generator\scripts\mobile\js"
+..\..\..\Blu82\bin\Debug\blu82.exe NVCG_configuration_inputs.xls
