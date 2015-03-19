c10b38af375fe4c6ff0c80e2385049e38aad0e3e
Bryan Pizzillo <bryanpizzillo@users.noreply.github.com>
2015-03-19 02:31:16 +0000

    -Added in output of Daisy "driver" xml File -removed "Press key to continue" for batch processing

---
+++ Input Generator/Blu82/MigrationTasks/MigrationDriver.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Blu82.MigrationTasks
+{
+    public class MigrationDriver
+    {
+        private List<MigrationTask> _tasks = new List<MigrationTask>();
+
+        public void Add(MigrationTask task)
+        {
+            _tasks.Add(task);
+        }
+
+        public string ToXML()
+        {
+            StringBuilder xml = new StringBuilder();
+
+            xml.Append("<?xml version=\"1.0\" encoding=\"utf-8\"?>\n");
+            xml.Append("<Migration xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\">\n");
+            xml.Append("\t<MigrationTaskList>\n");
+            foreach (MigrationTask task in _tasks)
+            {
+                xml.Append(task.OutputXML());
+            }
+            xml.Append("\t</MigrationTaskList>\n");
+            xml.Append("</Migration>\n");
+            return xml.ToString();
+
+        }
+
+    }
+}
---
+++ Input Generator/Blu82/MigrationTasks/MigrationTask.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Blu82.MigrationTasks
+{
+    public enum MigrationTaskTypes
+    {
+        Reset,
+        FolderCreator,
+        GeneralContentCreator,
+        RelationshipCreator,
+        ContentUpdater,
+        LinkToFolder,
+        Transitioner,
+        TranslationRelationshipCreator
+    }
+
+    public class MigrationTask
+    {
+        public MigrationTaskTypes MigrationTaskType { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Filename { get; set; }
+
+        public string OutputXML()
+        {
+            if (String.IsNullOrEmpty(Name))
+            {
+                throw new NullReferenceException("MigrationTask Name must not be null");
+            }
+
+            StringBuilder xml = new StringBuilder();
+
+            //Reset is wierd, so we will do this separately
+            if (MigrationTaskType != MigrationTaskTypes.Reset)
+            {
+                xml.Append(
+                    String.Format(
+                        "\t\t<MigrationTask xsi:type=\"{0}\" Name=\"{1}\">\n",
+                        MigrationTaskType.ToString(),
+                        Name
+                ));
+
+                string datagetter = "";
+                string datamapper = "";
+
+                switch (MigrationTaskType)
+                {
+                    case MigrationTaskTypes.GeneralContentCreator:
+                        datagetter = "XmlDataGetterOfFullItemDescription";
+                        datamapper = "XmlFullItemDescriptionMapper";
+                        break;                        
+                    case MigrationTaskTypes.ContentUpdater:
+                        datagetter = "XmlDataGetterOfUpdateContentItem";
+                        datamapper = "XmlUpdateItemMapper";
+                        break;                        
+                    case MigrationTaskTypes.FolderCreator:
+                        datagetter = "XmlDataGetterOfFolderDescription";
+                        datamapper = "XmlFolderDescriptionMapper";
+                        break;
+                    case MigrationTaskTypes.LinkToFolder:
+                        datagetter = "XmlDataGetterOfFolderLinkDescription";
+                        datamapper = "XmlFolderLinkDescriptionMapper";
+                        break;
+                    case MigrationTaskTypes.RelationshipCreator:
+                        datagetter = "XmlDataGetterOfRelationshipDescription";
+                        datamapper = "XmlRelationshipDescriptionMapper";
+                        break;
+                    case MigrationTaskTypes.TranslationRelationshipCreator:
+                        datagetter = "XmlDataGetterOfTranslationDescription";
+                        datamapper = "XmlTranslationDescriptionMapper";
+                        break;
+                    case MigrationTaskTypes.Transitioner:
+                        datagetter = "XmlDataGetterOfContentTypeTransitionDescription";
+                        datamapper = "XmlContentTypeTransitionDescriptionMapper";
+                        break;                        
+                }
+
+                if (!string.IsNullOrEmpty(datagetter) && !string.IsNullOrEmpty(datamapper))
+                {
+                    if (String.IsNullOrEmpty(Name))
+                    {
+                        throw new NullReferenceException("MigrationTask Filename must not be null");
+                    }
+
+                    xml.Append(String.Format(
+                        "\t\t\t<DataGetter xsi:type=\"{0}\" FileName=\"{1}\">\n", 
+                        datagetter,
+                        Filename));
+                    xml.Append(String.Format("\t\t\t\t<Mapper xsi:type=\"{0}\" />\n", datamapper));
+                    xml.Append("\t\t\t</DataGetter>\n");
+
+                }
+                else
+                {
+                    Console.WriteLine("Unknown MigrationTask");
+                    return "";
+                }
+            }
+            else
+            {
+                xml.Append(
+                    String.Format(
+                        "\t\t<MigrationTask xsi:type=\"{0}\" Name=\"{1}\" MonikerReset=\"true\">\n",
+                        MigrationTaskType.ToString(),
+                        Name
+                ));
+            }
+
+            xml.Append("\t\t</MigrationTask>\n");
+
+            return xml.ToString();
+        }
+    }
+}
