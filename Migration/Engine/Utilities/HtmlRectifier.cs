29070c1aaf9a7061d8d95898f6e4b70b6c85e117
Blair Learn <blairlearn@users.noreply.github.com>
2018-10-09 13:25:49 -0400

    Remove HtmlAgilityPack

--- Migration/Engine/Utilities/HtmlRectifier.cs
+++ Migration/Engine/Utilities/HtmlRectifier.cs
@@ -2,9 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using HtmlAgilityPack;
-using System.Web;
-using System.Web.UI;
 
 using NCI.CMS.Percussion.Manager.CMS;
 using Munger;
@@ -13,37 +10,37 @@ namespace MigrationEngine.Utilities
 {
     static class FieldHtmlRectifier
     {
+        // List of fields containing HTML content.
         static string[] fieldToBeRectified = { "bodyfield", "contact_text", "contact", "additional_information" };
 
         public static Dictionary<string, string> ConvertToXHtml(String UniqueIdentifier, Dictionary<string, string> fields, IMigrationLog logger, CMSController controller)
         {
             Dictionary<string, string> outgoing = new Dictionary<string, string>();
-            HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
-            doc.OptionOutputAsXml = true;
+            //HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+            //doc.OptionOutputAsXml = true;
             UrlMunger munger = new UrlMunger(controller);
 
             foreach (KeyValuePair<string, string> field in fields)
             {
+                // Is this one of the fields which is supposed to contain HTML?
                 if (fieldToBeRectified.Contains(field.Key))
                 {
-                    //For Testing 
-                    //string HtmlOut = HtmlEntity.DeEntitize("<p>Text</a></table>");
-                    
-                    string HtmlOut = HtmlEntity.DeEntitize(field.Value);
+                    // TODO: Convert HTML entities to Unicode.
+                    string HtmlOut = field.Value;
 
-                    //HTML to XML
-                    doc.LoadHtml(HtmlEntity.DeEntitize(HtmlOut));
-                    if (doc.ParseErrors.Count<HtmlParseError>() > 0)
-                    {
-                        StringBuilder sb = new StringBuilder();
-                        sb.Append("HTML to XML errors(corrected): \n");
-                        foreach (HtmlParseError err in doc.ParseErrors)
-                        {
-                            sb.AppendFormat("Line({0}): {1}\n", err.Line.ToString(), err.Reason);
-                        }
-                        logger.LogTaskItemWarning(UniqueIdentifier, sb.ToString(), null);
-                    }
-                    HtmlOut = doc.DocumentNode.InnerHtml;
+                    // TODO: Get the cleaned  up HTML and record any errors.
+                    //doc.LoadHtml(HtmlEntity.DeEntitize(HtmlOut));
+                    //if (doc.ParseErrors.Count<HtmlParseError>() > 0)
+                    //{
+                    //    StringBuilder sb = new StringBuilder();
+                    //    sb.Append("HTML to XML errors(corrected): \n");
+                    //    foreach (HtmlParseError err in doc.ParseErrors)
+                    //    {
+                    //        sb.AppendFormat("Line({0}): {1}\n", err.Line.ToString(), err.Reason);
+                    //    }
+                    //    logger.LogTaskItemWarning(UniqueIdentifier, sb.ToString(), null);
+                    //}
+                    //HtmlOut = doc.DocumentNode.InnerHtml;
 
                     //Munge
                     if (HtmlOut.Trim().Length > 0)
@@ -65,7 +62,7 @@ namespace MigrationEngine.Utilities
                 }
                 else
                 {
-                    //Build output list 
+                    //Copy unaltered field to output list.
                     outgoing.Add(field.Key, field.Value);
                 }
 
