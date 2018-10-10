5fd16b57119de85e7fde760f2847bcf1007e3a1b
Blair Learn <blairlearn@users.noreply.github.com>
2018-10-10 12:14:22 -0400

    Retrieve locale from content items.

---
+++ Migration/Engine.Tests/Descriptors/ContentDescriptionBase.Test.cs
@@ -0,0 +1,47 @@
+﻿using MigrationEngine;
+using MigrationEngine.Descriptors;
+using Xunit;
+
+namespace Engine.Tests.Descriptors
+{
+    public class ContentDescriptionBaseTest
+    {
+        /// <summary>
+        /// Verify the Locale property works correctly.
+        /// </summary>
+        /// <param name="locale"></param>
+        [Theory]
+        [InlineData("en-us")]
+        [InlineData("es-us")]
+        [InlineData("zh-cn")]
+        public void GetLocale(string locale)
+        {
+            // Minimal setup.
+            ContentDescriptionBase item = new FullItemDescription() {
+                ContentType = "TestType",
+                UniqueIdentifier = "17",
+            };
+            item.Fields.Add("sys_lang", locale);
+
+            Assert.Equal(locale, item.Locale);
+        }
+
+        /// <summary>
+        /// Verify the Locale property fails when sys_lang isn't set.
+        /// </summary>
+        [Fact]
+        public void GetMissingLocale()
+        {
+            // Minimal setup.
+            ContentDescriptionBase item = new FullItemDescription()
+            {
+                ContentType = "TestType",
+                UniqueIdentifier = "17",
+            };
+
+            Assert.Throws<DataFieldException>( () => {
+                string locale = item.Locale;
+            });
+        }
+    }
+}
--- Migration/Engine/Descriptors/ContentDescriptionBase.cs
+++ Migration/Engine/Descriptors/ContentDescriptionBase.cs
@@ -8,6 +8,8 @@ namespace MigrationEngine.Descriptors
 {
     abstract public class ContentDescriptionBase : MigrationData
     {
+        const string LOCALE_FIELD = "sys_lang";
+
         public String ContentType { get; set; }
         public String UniqueIdentifier { get; set; }
 
@@ -18,6 +20,17 @@ namespace MigrationEngine.Descriptors
                 string fmt = @"<UniqueIdentifier value=""{0}""/><ContentType value=""{1}""/>";
 
                 return string.Format(fmt, UniqueIdentifier, ContentType);
+            }
+        }
+
+        public string Locale
+        {
+            get
+            {
+                if (this.Fields.ContainsKey(LOCALE_FIELD))
+                    return this.Fields[LOCALE_FIELD];
+
+                throw new DataFieldException("Locale not set.");
             }
         }
     }
